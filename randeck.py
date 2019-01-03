2e6a403860dc000f66e915d0e1bcfbfbece77d28
sooobus <sooobus@gmail.com>
2019-01-03 17:10:12 +0300

    fix bug'

--- randeck.py
+++ randeck.py
@@ -8,6 +8,6 @@ decks = decks_base + decks_ktulhu + decks_plants + decks_mimimi
 chosen = sample(decks, 4)
 #print("Ваня: " + chosen[0] + "-" + chosen[1] + "; Лера: " + chosen[2] + "-" + chosen[3])
 
-chosen = sample(decks_ktulhu + decks_plants + decks_mimimi, 2)
+chosen = sample(decks_ktulhu + decks_plants + decks_base, 2)
 chosen_mimimi = sample(decks_mimimi, 2)
-print("Ваня: " + chosen[0] + "-" + chosen_mimimi[0] + "; Лера: " + chosen[1] + "-" + chosen_mimimi[2])
+print("Ваня: " + chosen[0] + "-" + chosen_mimimi[0] + "; Лера: " + chosen[1] + "-" + chosen_mimimi[1])
