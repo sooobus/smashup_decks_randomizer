04a85699d1bfe7f957adbad1474c49a42135d0df
sooobus <sooobus@gmail.com>
2019-01-03 17:08:02 +0300

    init

---
+++ randeck.py
@@ -0,0 +1,13 @@
+from random import sample
+
+decks_base = ['пираты', 'зомби', 'роботы', 'маги', 'динозавры', 'ниндзя', 'лепреконы', 'пришельцы']
+decks_ktulhu = ['старцы', 'учёные', 'ктулху', 'местные']
+decks_plants = ['растения', 'медведи', 'механики', 'призраки']
+decks_mimimi = ['принцессы' ,'котики', 'феи', 'пони']
+decks = decks_base + decks_ktulhu + decks_plants + decks_mimimi
+chosen = sample(decks, 4)
+#print("Ваня: " + chosen[0] + "-" + chosen[1] + "; Лера: " + chosen[2] + "-" + chosen[3])
+
+chosen = sample(decks_ktulhu + decks_plants + decks_mimimi, 2)
+chosen_mimimi = sample(decks_mimimi, 2)
+print("Ваня: " + chosen[0] + "-" + chosen_mimimi[0] + "; Лера: " + chosen[1] + "-" + chosen_mimimi[2])
